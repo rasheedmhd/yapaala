eeaff901e5ca20968459b1ee579a7d3c18d5a1b8
Rasheed Starlet <rasheedcoder@gmail.com>
2019-03-20 11:41:30 +0000

    HC2.1

--- yiya/settings.py
+++ yiya/settings.py
@@ -132,7 +132,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
